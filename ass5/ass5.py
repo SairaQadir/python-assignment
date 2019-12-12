71d9ec300e94c28a4ea1d0b1d3f29955ef4ec7c8
saira <syraqadir12@gmail.com>
2019-12-12 22:40:21 +0500

    py

---
+++ ass5/ass5.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Input a number to compute the factiorial : "))
+print(factorial(n))
+
+
+# In[3]:
+
+
+def string_test(s):
+    d={"UPPER_CASE":0, "LOWER_CASE":0}
+    for c in s:
+        if c.isupper():
+           d["UPPER_CASE"]+=1
+        elif c.islower():
+           d["LOWER_CASE"]+=1
+        else:
+           pass
+    print ("Original String : ", s)
+    print ("No. of Upper case characters : ", d["UPPER_CASE"])
+    print ("No. of Lower case Characters : ", d["LOWER_CASE"])
+
+string_test('Man In Black')
+
+
+# In[4]:
+
+
+def is_even_num(l):
+    enum = []
+    for n in l:
+        if n % 2 == 0:
+            enum.append(n)
+    return enum
+print(is_even_num([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+
+# In[5]:
+
+
+def isPalindrome(string):
+	left_pos = 0
+	right_pos = len(string) - 1
+	
+	while right_pos >= left_pos:
+		if not string[left_pos] == string[right_pos]:
+			return False
+		left_pos += 1
+		right_pos -= 1
+	return True
+print(isPalindrome('aza')) 
+
+
+# In[6]:
+
+
+def test_prime(n):
+    if (n==1):
+        return False
+    elif (n==2):
+        return True;
+    else:
+        for x in range(2,n):
+            if(n % x==0):
+                return False
+        return True             
+print(test_prime(9))
+
+
+# In[7]:
+
+
+def cart(*items):
+    for i in items:
+        print("you bought:", i)
+cart("shirt", "trouser", "shoes")
+cart("coat")
+cart("bracelet")
+cart("haircolor")
+cart("shirt", "trouser", "shoes","coat", "bracelet","haircolor")
+
+
+# In[ ]:
+
+
+
+
